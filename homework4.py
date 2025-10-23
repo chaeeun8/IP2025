a7451a541b6bec2a2ff4d5b24aec5a13dbfd6b2a
chaeeun8 <129680997+chaeeun8@users.noreply.github.com>
2025-10-23 23:23:43 +0900

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,37 @@
+import cv2
+import numpy as np
+
+def run_main():
+    cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+    while(True):
+        ret, frame = cap.read()
+        roi = frame[0:500, 0:500]
+        gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+        gray_blur = cv2.GaussianBlur(gray, (9, 9), 2)
+        thresh = cv2.adaptiveThreshold(gray_blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                       cv2.THRESH_BINARY_INV, 21, 2)
+        kernel = np.ones((5, 5), np.uint8)
+        closing = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE,
+        kernel, iterations=2)
+        cont_img = closing.copy()
+        contours, hierarchy = cv2.findContours(cont_img, cv2.RETR_EXTERNAL,
+                                               cv2.CHAIN_APPROX_SIMPLE)
+        for cnt in contours:
+            area = cv2.contourArea(cnt)
+            if area < 2000 or area > 5000:
+                continue
+            if len(cnt) < 5:
+                continue
+            ellipse = cv2.fitEllipse(cnt)
+            cv2.ellipse(roi, ellipse, (0,255,0), 2)
+        cv2.imshow("Morphological Closing", closing)
+        cv2.imshow("Adaptive Thresholding", thresh)
+        cv2.imshow('Contours', roi)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+if __name__ == "__main__":
+    run_main()
